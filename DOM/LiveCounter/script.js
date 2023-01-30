930bc46458c7af051dba6bf9db8d814b739de558
Swatantra chaurasia <chaurasiyaswatantra65@gmail.com>
2023-01-30 23:59:45 +0530

    Adding DOM files to repository

---
+++ DOM/LiveCounter/script.js
@@ -0,0 +1,18 @@
+const counter = document.querySelectorAll(".counter")
+counter.forEach((count)=>{
+    count.innerText = "0"
+
+    const updateCounter = ()=>{
+        const target = count.getAttribute("data-target")
+        const c = +count.innerText;
+        const increment = target/200;
+
+        if(c<target){
+            count.innerText = `${Math.ceil(c+increment)}`
+            setTimeout(updateCounter, 1)
+        }else{
+            count.innerText = target
+        }
+    }
+    updateCounter()
+})
---
+++ DOM/QuestionBank/script.js
@@ -0,0 +1,6 @@
+const toggle = document.querySelectorAll(".faq-toggle")
+toggle.forEach(T =>{
+    T.addEventListener("click",()=>{
+        T.parentNode.classList.toggle("active")
+    })
+})
