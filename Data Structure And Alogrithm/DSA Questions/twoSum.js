cc918cf34db884daa2751ff065a6682132ba6341
Swatantra chaurasia <chaurasiyaswatantra65@gmail.com>
2023-03-02 10:41:05 +0530

    Find Sum DSA

---
+++ Data Structure And Alogrithm/DSA Questions/twoSum.js
@@ -0,0 +1,19 @@
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+
+
+var num = [2,7,11,15] 
+target = 9
+
+let twoSum = function (nums, target){
+    for(let i = 0;i < nums.length; i++){
+        for(let j = i + 1; i< nums.length; i++){
+            if (nums[i] + nums[j] === target ){
+                return [i , j]
+            }
+        }
+    }
+}
+console.log(twoSum(num, target))
