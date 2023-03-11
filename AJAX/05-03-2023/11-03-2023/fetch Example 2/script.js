a47fb099ef9aeaf7fdd97ea519ae91f6a8ee954a
Swatantra chaurasia <chaurasiyaswatantra65@gmail.com>
2023-03-11 18:30:53 +0530

    CSS effect added

--- AJAX/05-03-2023/11-03-2023/fetch Example 2/script.js
+++ AJAX/05-03-2023/11-03-2023/fetch Example 2/script.js
@@ -15,15 +15,18 @@ function show(data){
     `
     <tr>
     <th>ID</th>
+    <th>Image</th>
     <th>Description</th>
     <th>Brand</th>
     <th>Price</th>
+    
     </tr>
     `;
 
     for(let r of data.products){
         tab += `<tr>
         <td>${r.id}</td>
+        <td><img src = "${r.images[0]}"</td>
         <td>${r.description}</td>
         <td>${r.brand}</td>
         <td>${r.price}</td>
@@ -32,3 +35,59 @@ function show(data){
     }
     document.getElementById("phone").innerHTML = tab
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// const API_URL = "https://dummyjson.com/products";
+
+//   async function getAPI(url) {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     console.log(data);
+
+//     show(data);
+//   }
+
+//   getAPI(API_URL);
+
+//   function show(data) {
+//     const container = document.getElementById("phone");
+
+//     for (const product of data.products) {
+//       const card = document.createElement("div");
+//       card.className = "card";
+//       container.appendChild(card);
+
+//       const img = document.createElement("img");
+//       img.src = product.images[1];
+//       card.appendChild(img);
+
+//       const details = document.createElement("div");
+//       details.className = "details";
+//       card.appendChild(details);
+
+//       const description = document.createElement("div");
+//       description.className = "description";
+//       description.textContent = product.description;
+//       details.appendChild(description);
+
+//       const brand = document.createElement("div");
+//       brand.className = "brand";
+//       brand.textContent = product.brand;
+//       details.appendChild(brand);
+
+//       const price = document.createElement("div");
+//       price.className = "price";
+//       price.textContent = `$${product.price}`;
+//       details.appendChild(price);
+//     }
+//   }
