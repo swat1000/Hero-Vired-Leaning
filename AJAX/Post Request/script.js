1438cef92cd7f7be2c8529ca64e61a34c8e19c79
Swatantra chaurasia <chaurasiyaswatantra65@gmail.com>
2023-03-17 15:35:59 +0530

    Post Request In AJAX

---
+++ AJAX/Post Request/script.js
@@ -0,0 +1,40 @@
+const createTodo = async (todo) => {
+
+
+    let options = {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(todo),
+
+    }
+    let p = await fetch('https://jsonplaceholder.typicode.com/posts', options)
+    let response = await p.json()
+    return response
+}
+
+const getTodo = async (id)=>{
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts/' + id)
+    let r =await response.json()
+    return r
+}
+
+    const mainFunc = async () => {
+        let todo = {
+            title: "Swatantra1",
+            body: "Chaurasia1",
+            userID: 1000,
+        }
+        let todor = await createTodo(todo)
+        console.log(todor)
+        console.log(await getTodo(101))
+
+
+    }
+
+
+mainFunc()
+
+
+
