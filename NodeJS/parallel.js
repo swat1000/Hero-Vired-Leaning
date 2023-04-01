a2817d4180002f83ebeb634a31d5325be125207d
Swatantra chaurasia <chaurasiyaswatantra65@gmail.com>
2023-04-01 19:52:29 +0530

    Node JS Tutorial

---
+++ NodeJS/parallel.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+console.log("start");
+
+fs.readFile("./node1.txt", "utf-8", (err, data)=>{
+   
+    console.log("we are inside callback function", data);
+})
+
+fs.readFile("./node2.txt", "utf-8", (err, data)=>{
+   
+    console.log("we are inside callback function", data);
+})
+
+fs.readFile("./node3.txt", "utf-8", (err, data)=>{
+    
+    console.log("we are inside callback function", data);
+})
+
+console.log("end");
---
+++ NodeJS/serial.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+console.log("start");
+fs.readFile("./nodejs.txt", "utf-8", (err, data)=>{
+    console.log("we are inside callback function", data);
+    fs.readFile("./nodejs.txt", "utf-8", (err, data)=>{
+        console.log("we are inside callback function", data);
+        fs.readFile("./nodejs.txt", "utf-8", (err, data)=>{
+            console.log("we are inside callback function", data);
+        })
+    })
+})
+
+console.log("end")
