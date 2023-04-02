0437d1ae066a13194b1895d349c9f98d978bfd5b
Swatantra chaurasia <chaurasiyaswatantra65@gmail.com>
2023-04-02 18:50:22 +0530

    File path __dirname

--- NodeJS/Filesystem/index.js
+++ NodeJS/Filesystem/index.js
@@ -12,12 +12,43 @@ const fs = require ("fs")
 
 
 // reading folder
-
 let folderPath = __dirname;
-fs.readdir(folderPath, "utf-8", (err, data)=>{
+fs.readdir(folderPath, "utf-8", (err, data) => {
     console.log(data);
-    //output of reading folder
+    // output of reading folder
     let arrayoffiles = data;
-    let readingdata = fs.readFileSync(arrayoffiles[2], "utf-8", )
-    console.log(readingdata, "reading the data of file");
+    for (let index = 0; index < arrayoffiles.length; index++) {
+        let isfile = fs.lstatSync(arrayoffiles[index]).isFile();
+        if (isfile) {
+            let data = fs.readFileSync(arrayoffiles[index], "utf-8");
+            console.log(data,"------------file reading----------");
+        }
+        let isfolder = fs.lstatSync(arrayoffiles[index]).isDirectory();
+        if (isfolder) {
+            //
+            let direcotrypath_of_this_file = path.dirname(isfolder);
+            console.log(direcotrypath_of_this_file);
+        }
+        
+    }
+    let filename = data[2];
+    let direcotrypath_of_this_file = path.extname(filename);
+    console.log(direcotrypath_of_this_file);
+
 })
+
+// whether it is directorty ;
+// if it is direcotry then go inside the directory and check if files are there or not;
+
+
+let filepath = __filename
+console.log(filepath);
+let diroffile = path.dirname(filepath);
+console.log(diroffile);
+
+
+
+
+
+
+
